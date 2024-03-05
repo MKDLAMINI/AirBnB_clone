c5b59d845c82d1fcb7dac2619094596435242787
MKDLAMINI <dlaminikhehla5@gmail.com>
2024-03-05 04:22:40 -0800

    user class defined, just need to update console.py to run User

---
+++ models/user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Creates the User class."""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """define a User."""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, **kwargs):
+        """Start a new User instance.
+
+        Args:
+            kwargs (dict): A dictionary of attributes for the user.
+        """
+        super().__init__(**kwargs)
+        self.email = kwargs.get("email", "")
+        self.password = kwargs.get("password", "")
+        self.first_name = kwargs.get("first_name", "")
+        self.last_name = kwargs.get("last_name", "")
